0adbcb2a3542a1352cdb034498324350e6035064
astakh <v.astakhov@gmail.com>
2021-12-06 11:32:24 +0200

    eeeeeeeeeeeeeeeee

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ document.querySelector(".js-invoke").addEventListener("click", async function ()
 
         // Read an answer from dApp data storage
 
-            let answer = await nodeInteraction.accountDataByKey(user.address+'_a',ballAddress,nodeUrl);
-            document.querySelector(".answer").innerHTML = `Your answer is: ${answer.value}`;
+            let answer = await nodeInteraction.accountDataByKey('11',ballAddress,nodeUrl);
+            document.querySelector(".answer").innerHTML = `Pixel 11 data: ${answer.value}`;
         } catch (e) {
             console.error('Question denied')
         }; 
